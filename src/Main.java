a63f45bcb840dd9c7ae45a472703681c22dacbaa
Vladimir444k <gluschenkovladimir337@gmail.com>
2023-05-08 19:20:56 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int amount = 250;
         int bonus = 20;
 
-        int mile = amount/bonus;
+        int mile = amount / bonus;
 
         System.out.println("Бонусных миль в итоге: " + mile);
     }
